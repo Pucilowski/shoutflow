27ccdf66218ad044f678e1d946abb249b0439c8b
Pucilowski <pucilowski.m@gmail.com>
2017-12-10 19:04:41 +0000

    -

--- shoutflow-domain/src/main/scala/com/pucilowski/shoutflow/domain/UserRepository.scala
+++ shoutflow-domain/src/main/scala/com/pucilowski/shoutflow/domain/UserRepository.scala
@@ -4,13 +4,16 @@ import java.util.UUID
 
 import com.pucilowski.shoutflow.events.UserCreated
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
 
 trait UserRepository {
   def get(id: UUID): Future[UserRoot]
 }
 
 class ConcreteUserRepository extends UserRepository {
+
+  implicit val executor: ExecutionContext =  scala.concurrent.ExecutionContext.global
+
   override def get(id: UUID): Future[UserRoot] = Future {
     UserRoot.seed(UserCreated(UUID.randomUUID(), "", "", ""))
   }
